ca539ee2509e50b2891837494b4608a8369c4cf6
shuo.zhang <zshowing@gmail.com>
2017-03-02 16:38:50 +0800

    Translation support.

--- cheater.py
+++ cheater.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 import sys
 import os
+import requests
 
 if len(sys.argv) < 3:
 	print "Not enough arguments!"
@@ -30,7 +32,19 @@ def checkMatch(word):
 
 for word in content:
 	if len(word) == count and checkValid(word) and checkMatch(word):
+		url = "http://fanyi.youdao.com/openapi.do?keyfrom=4Pics1WordCheater&key=1630335074&type=data&doctype=json&version=1.1&q=%s" % word
+		r = requests.get(url)
+		response = r.json()
+		# for trans in response['translation']:
+		# 	print trans
+		if 'basic' in response and 'explains' in response['basic']:
+			for explain in response['basic']['explains']:
+				print explain
+		else:
+			continue
+		
 		print word
+		print ""
 		match += 1
 
 print match
